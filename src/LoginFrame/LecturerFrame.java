f2edc1f76f25e4606c5114c7d189d4f54b24ca00
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2020-12-25 16:09:38 +0530

    Lecturer Details Update

--- src/LoginFrame/LecturerFrame.java
+++ src/LoginFrame/LecturerFrame.java
@@ -104,10 +104,10 @@ public class LecturerFrame extends javax.swing.JFrame {
         jButton5 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jLabel8 = new javax.swing.JLabel();
-        txtEFname = new javax.swing.JTextField();
-        txtEFname1 = new javax.swing.JTextField();
-        txtEFname2 = new javax.swing.JTextField();
-        txtEFname3 = new javax.swing.JTextField();
+        LFname = new javax.swing.JTextField();
+        LLname = new javax.swing.JTextField();
+        LAddress = new javax.swing.JTextField();
+        LContact = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
         jLabel37 = new javax.swing.JLabel();
         jLabel38 = new javax.swing.JLabel();
@@ -305,6 +305,11 @@ public class LecturerFrame extends javax.swing.JFrame {
         jLabel39.setText("Contact");
 
         jButton7.setText("Update");
+        jButton7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton7ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -324,10 +329,10 @@ public class LecturerFrame extends javax.swing.JFrame {
                             .addComponent(jLabel39))
                         .addGap(74, 74, 74)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtEFname3)
-                            .addComponent(txtEFname2)
-                            .addComponent(txtEFname1)
-                            .addComponent(txtEFname)))
+                            .addComponent(LContact)
+                            .addComponent(LAddress)
+                            .addComponent(LLname)
+                            .addComponent(LFname)))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jButton7)))
@@ -340,19 +345,19 @@ public class LecturerFrame extends javax.swing.JFrame {
                 .addComponent(jLabel8)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtEFname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LFname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel9))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtEFname1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LLname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel37))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtEFname2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LAddress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel38))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtEFname3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LContact, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel39))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton7)
@@ -423,6 +428,17 @@ public class LecturerFrame extends javax.swing.JFrame {
         lecturerGPAFrame.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
+    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
+        // TODO add your handling code here:
+        String fname = LFname.getText();
+        String lname = LLname.getText();
+        String address = LAddress.getText();
+        String contact = LContact.getText();
+        
+       LectureDetails lectureDetails = new LectureDetails(fname,lname,address,contact);
+       lectureDetails.updateData();
+    }//GEN-LAST:event_jButton7ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -459,6 +475,10 @@ public class LecturerFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField LAddress;
+    private javax.swing.JTextField LContact;
+    private javax.swing.JTextField LFname;
+    private javax.swing.JTextField LLname;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
@@ -490,9 +510,5 @@ public class LecturerFrame extends javax.swing.JFrame {
     private javax.swing.JLabel lblLName;
     private java.awt.List listLNotice;
     private java.awt.Panel panel1;
-    private javax.swing.JTextField txtEFname;
-    private javax.swing.JTextField txtEFname1;
-    private javax.swing.JTextField txtEFname2;
-    private javax.swing.JTextField txtEFname3;
     // End of variables declaration//GEN-END:variables
 }
